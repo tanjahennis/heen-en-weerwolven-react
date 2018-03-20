5ef2a7498449d65be2775c179041dc9933e9ec9b
Tanja Hennis <tmhennis@gmail.com>
2018-03-20 14:59:33 +0100

    styling nameBlock

--- src/containers/Village.js
+++ src/containers/Village.js
@@ -4,44 +4,25 @@ import PlayerDialog from '../components/games/PlayerDialog'
 import MayorMedal from '../images/mayor-medal.png'
 import VillageMenuButton from '../components/games/VillageMenuButton'
 import movePlayers from '../actions/games/move'
-
 import EmailIcon from 'material-ui/svg-icons/communication/email'
 import './Village.css'
 
-const setClassName = ( mayor, receivedMessages, dead) => {
-  if (mayor) {
-      return 'sidebar-mayor'
-  }
-  if ( receivedMessages.length < 0){
-    return ''
-  } else if (receivedMessages.length > 0){
-    return 'sidebar-mail'
-  }
-
-  if (dead) {
-    return 'sidebar-dead'
-  }
-}
-
 class Village extends PureComponent {
+
   renderPlayer(player, index) {
     let unreadMessages = player.receivedMessages.filter(function(message){
       return message.messageRead === false
     })
 
     return(
-      <div key={index} className={setClassName(player.mayor, player.receivedMessages, player.dead)}>
-        <div className='sidebar-name'>
-          <div className='nameblock'>
-            <p>
-              {player.name}
-            </p>
-            <span>
-              {player.mayor && player.dead === false ? <img src={MayorMedal} className="medal-sidebar" alt="MayorMedal" /> : ''}
-              <span className="email-sidebar">{player.receivedMessages.length && unreadMessages.length > 0 && player.dead === false ? <EmailIcon /> : ''}</span>
-            </span>
+      <div key={index}>
+        <div className='sidebarName'>
+          <div className='nameBlock'>
+            <div className='playerName'>{player.name}</div>
+            {player.mayor && player.dead === false ? <img src={MayorMedal} className="medalSidebar" alt="MayorMedal" /> : ''}
+            {player.receivedMessages.length && unreadMessages.length > 0 && player.dead === false ? <EmailIcon /> : ''}
           </div>
-          <div className='pop-over'>
+          <div className='popOver'>
             {<PlayerDialog player={player} {...player.messageSent} /> }
           </div>
         </div>
