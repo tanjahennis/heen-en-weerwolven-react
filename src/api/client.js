bf349e9c6d4302d7b43e768fc1109b9f977145cf
Tanja Hennis <tmhennis@gmail.com>
2018-03-24 15:35:49 +0100

    update api client

--- src/api/client.js
+++ src/api/client.js
@@ -7,7 +7,7 @@ export default class ApiClient {
 
   constructor(host, options = {}) {
     this.host = process.env.NODE_ENV === 'production'
-      ? 'https://werewolves-react.herokuapp.com'
+      ? 'https://werewolven.herokuapp.com'
       : (host || 'http://localhost:3030')
 
     this.options = { ...this.defaultOptions, ...options }
