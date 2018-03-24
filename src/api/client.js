edecc905b2ca7cab977de8c6e3ce7bf6df5abd96
Tanja Hennis <tmhennis@gmail.com>
2018-03-24 14:23:16 +0100

    Make API host configurable via API_HOST

--- src/api/client.js
+++ src/api/client.js
@@ -7,7 +7,7 @@ export default class ApiClient {
 
   constructor(host, options = {}) {
     this.host = process.env.NODE_ENV === 'production'
-      ? 'https://heen-en-weerwolven-api.herokuapp.com'
+      ? 'https://mighty-escarpment-47958.herokuapp.com/'
       : (host || 'http://localhost:3030')
 
     this.options = { ...this.defaultOptions, ...options }
